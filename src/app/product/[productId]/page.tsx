4be75eefc42f537a17ee21c9d57955f644a90efe
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-02-20 10:36:44 +0100

    added description and btn

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -21,6 +21,18 @@ export default async function ProductDetailsPage(props:{params:Params}) {
     notFound();
   }
     return (
-    <div>Details</div>
+    <div className="space-y-4 p-4 max-w-5xl mx-auto">
+        <h1 className="text-4xl font-bold">{product.name}</h1>
+        <p>${product.price.toNumber()}</p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni dolores tenetur perferendis maxime suscipit error unde exercitationem repudiandae laudantium animi ipsum architecto totam molestias dolorum numquam, est doloribus veritatis?
+        </p>
+        {product.inStock? (
+            <p className="text-green-500">Available</p>
+        ): (
+            <p className="text-red-500">Out of stock</p>
+        )}
+        <button className="text-center px-3 py-2 rounded-lg bg-slate-900 text-white">Add to cart</button>
+    </div>
   )
 }
