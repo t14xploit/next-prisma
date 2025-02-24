baa6d1a8fb66e3c9de0a1922b739275eefaf94dd
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-02-24 11:42:47 +0100

    added to db

--- src/app/product/create/actions.ts
+++ src/app/product/create/actions.ts
@@ -1,10 +1,30 @@
 "use server";
 
+import { redirect } from "next/navigation";
+import {prisma} from "@/lib/prisma"
 
 export  async function createProduct(prevState:unknown, formData: FormData) {
-const obj = Object.fromEntries(formData.entries());
-console.log(obj);
-return{
-    message:"Product created!",
-};
+// const obj = Object.fromEntries(formData.entries());
+// console.log(obj);
+
+const name = formData.get("name") as string;
+const price = Number(formData.get("price") as string);
+
+try{
+    await prisma.product.create({
+        data:{
+            name,
+            price,
+        },
+    });
+} catch(error){
+    console.log(error);
+    return{
+        message: "Failed to create a product!",
+    };
+}
+
+ redirect("/");
+
+
 }
