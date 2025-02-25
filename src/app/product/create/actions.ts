c364e765d14e09fc0cbc754ca21c1db0e549a8a4
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-02-25 10:09:19 +0100

    installed zod

--- src/app/product/create/actions.ts
+++ src/app/product/create/actions.ts
@@ -2,13 +2,24 @@
 
 import { redirect } from "next/navigation";
 import {prisma} from "@/lib/prisma"
+import {z} from "zod";
+
+const createSchema = z.object({
+    name:z.string().nonempty(),
+    price:z.coerce.number().min(0),
+    description:z.string().max(1000).optional(),
+    inStock:z.coerce.boolean().default(false),
+    image:z.string().url().optional(),
+});
 
 export  async function createProduct(prevState:unknown, formData: FormData) {
-// const obj = Object.fromEntries(formData.entries());
+const obj = Object.fromEntries(formData.entries());
+
+const result = createSchema.safeParse(obj);
 // console.log(obj);
 
-const name = formData.get("name") as string;
-const price = Number(formData.get("price") as string);
+// const name = formData.get("name") as string;
+// const price = Number(formData.get("price") as string);
 
 try{
     await prisma.product.create({
