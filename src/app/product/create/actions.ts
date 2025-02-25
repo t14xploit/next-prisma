91bdcabc5618c51fcc520a0a92358025030cf8fb
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-02-25 10:40:29 +0100

    fixed prisma create func. added img and descr could be null also

--- src/app/product/create/actions.ts
+++ src/app/product/create/actions.ts
@@ -1,41 +1,54 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import {prisma} from "@/lib/prisma"
-import {z} from "zod";
+import { prisma } from "@/lib/prisma"
+import { z } from "zod";
 
 const createSchema = z.object({
-    name:z.string().nonempty(),
-    price:z.coerce.number().min(0),
-    description:z.string().max(1000).optional(),
-    inStock:z.coerce.boolean().default(false),
-    image:z.string().url().optional(),
+    name: z.string().nonempty(),
+    price: z.coerce.number().min(0),
+    description: z.string().max(1000).optional(),
+    inStock: z.coerce.boolean().default(false),
+    image: z.union([z.string().url(), z.literal("")]),
+
 });
 
-export  async function createProduct(prevState:unknown, formData: FormData) {
-const obj = Object.fromEntries(formData.entries());
+export async function createProduct(prevState: unknown, formData: FormData) {
+    const obj = Object.fromEntries(formData.entries());
 
-const result = createSchema.safeParse(obj);
-// console.log(obj);
+    const result = createSchema.safeParse(obj);
+    if (!result.success) {
 
-// const name = formData.get("name") as string;
-// const price = Number(formData.get("price") as string);
+        console.log(result.error.flatten());
+        return {
+            message: "Failed to create a product!",
+            error: result.error.message,
+        };
+    }
 
-try{
-    await prisma.product.create({
-        data:{
-            name,
-            price,
-        },
-    });
-} catch(error){
-    console.log(error);
-    return{
-        message: "Failed to create a product!",
-    };
-}
 
- redirect("/");
+
+    // console.log(obj);
+
+    // const name = formData.get("name") as string;
+    // const price = Number(formData.get("price") as string);
+
+    try {
+        await prisma.product.create({
+            data: {
+                ...result.data,
+                image: result.data.image || null,
+                description: result.data.description || null
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return {
+            message: "Failed to create a product!",
+        };
+    }
+
+    redirect("/");
 
 
 }
